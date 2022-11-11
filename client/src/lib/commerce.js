6a742d733ff0454e426387517e8c217a012ab204
Lucas Faustman <lucasfaustman@gmail.com>
2022-11-11 15:59:45 -0800

    Added Stripe integration

--- client/src/lib/commerce.js
+++ client/src/lib/commerce.js
@@ -1,7 +1,6 @@
 import Commerce from '@chec/commerce.js';
 
 const checAPIKey = process.env.REACT_APP_CHEC_PUBLIC_KEY;
-console.log(process.env.REACT_APP_CHEC_PUBLIC_KEY)
 const devEnvironment = process.env.NODE_ENV === 'development';
 
 const commerceConfig = {
