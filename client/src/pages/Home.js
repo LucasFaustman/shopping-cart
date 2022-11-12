b20a044527230cc428353e3284d0a45ca8ea8c27
Lucas Faustman <lucasfaustman@gmail.com>
2022-11-12 15:15:59 -0800

    Edited file names for better readability/maintainability

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,11 @@
-import Hero from "../components/Hero"
+import Hero from "../components/Landing-Hero"
 import Navbar from "../components/Navbar"
-import BestSellers from "../components/BestSellers"
+import BestSellers from "../components/Landing-BestSellers"
 import Footer from "../components/Footer"
-import ProductPoints from "../components/ProductPoints"
-import ProductFeature from "../components/ProductFeature"
-import Testimonials from "../components/Testimonials"
-import CallToAction from "../components/CallToAction"
+import ProductPoints from "../components/Landing-ProductPoints"
+import ProductFeature from "../components/Landing-ProductFeature"
+import Testimonials from "../components/Landing-Testimonials"
+import CallToAction from "../components/Landing - CallToAction"
 
 export default function Home({products}) {
     return (
