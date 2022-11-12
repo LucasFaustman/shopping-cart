aafb8559e59694af7e9a73f7eedbf7b3a518286a
Lucas Faustman <lucasfaustman@gmail.com>
2022-11-12 15:22:42 -0800

    Added package.json to root folder

--- server/server.js
+++ server/server.js
@@ -1,6 +1,11 @@
 const e = require("express");
 const express = require("express");
 const app = express();
+// server/index.js
+const path = require('path');
+
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 const env = require("dotenv").config();
 // This is your test secret API key.
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
